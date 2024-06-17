94815b8c52d6830499b52f4e7f025a67062387e3
Shinichiro Oba <bricklife@gmail.com>
2024-06-17 13:06:11 +0900

    Use Swift 6 mode in Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -120,5 +120,6 @@ let package = Package(
         .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
       ]
     ),
-  ]
+  ],
+  swiftLanguageVersions: [.v6]
 )
